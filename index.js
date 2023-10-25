1b332cc56b63f92f7e89eca992ef6497522f9622
alexandre <alexandre.coutinho81@hotmail.com>
2023-10-25 16:07:34 -0300

    desafio3

---
+++ index.js
@@ -0,0 +1,35 @@
+class heroi{
+    constructor(nome, idade, tipo, ataque){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+        this.ataque = ataque
+    }
+
+    atacar() {
+        let ataque = "";
+    
+        switch (this.tipo) {
+          case "mago":
+            ataque = "usou magia";
+            break;
+          case "guerreiro":
+            ataque = "usou espada";
+            break;
+          case "monge":
+            ataque = "usou artes marciais";
+            break;
+          case "ninja":
+            ataque = "usou shuriken";
+            break;
+          default:
+            ataque = "usou um ataque desconhecido";
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+let ataque = new heroi("", "", "monge", "")
+
+ataque.atacar()
